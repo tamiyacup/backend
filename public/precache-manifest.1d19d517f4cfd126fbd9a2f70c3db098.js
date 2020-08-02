9134241958db7b201b99e55cbc60abaeb9a6a75c
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-08-02 12:22:35 +0700

    react pages

---
+++ public/precache-manifest.1d19d517f4cfd126fbd9a2f70c3db098.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf2566e1c8c198849c31b070cfcf66f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81fb11adbc9de2fd6996",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "33c5f01a1894f9a5fd7c",
+    "url": "/static/css/main.3eebce9f.chunk.css"
+  },
+  {
+    "revision": "81fb11adbc9de2fd6996",
+    "url": "/static/js/2.441fe41a.chunk.js"
+  },
+  {
+    "revision": "ffa9e0eab19ab3d1f001172cb8e754cc",
+    "url": "/static/js/2.441fe41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c5f01a1894f9a5fd7c",
+    "url": "/static/js/main.7181cbe5.chunk.js"
+  },
+  {
+    "revision": "1898dca51257845b1bd0",
+    "url": "/static/js/runtime-main.70f33833.js"
+  },
+  {
+    "revision": "1ca1d0d327ee7decefb5520b775fb0e0",
+    "url": "/static/media/ComicMono.1ca1d0d3.ttf"
+  },
+  {
+    "revision": "d497a8b3c84a0c9f9f916a668f12b749",
+    "url": "/static/media/Sarabun-Medium.d497a8b3.ttf"
+  },
+  {
+    "revision": "b5e067b725dcc482da7337ac5964cc51",
+    "url": "/static/media/amp.b5e067b7.jpg"
+  }
+]);
