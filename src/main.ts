f8bfecc56831fb9d52fa277d5c5617250ce2c66e
NantipatSoften <keymasterviriya1150@gmail.com>
2020-08-05 09:32:54 +0700

    🔨   fixing a bug or issue

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as helmet from "helmet";
-import * as express from 'express'
+import * as express from "express";
 import { join } from "path";
+declare const module: any;
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(helmet());
     app.enableCors();
     app.use("/public", express.static(join(__dirname, "..", "/uploads")));
     await app.listen(3002);
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 bootstrap();
